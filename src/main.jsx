4e790cefd277b49328995e10809609d1e96134d0
Satrio <SatrioKece12@users.noreply.github.com>
2026-01-31 11:15:41 +0700

    fix aos & root

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,37 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import PreLoader from './components/PreLoader.jsx'
-import "animate.css"
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
+import 'animate.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+function Root() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 800,
+      easing: 'ease-in-out',
+    })
+  }, [])
+
+  return (
+    <>
+      <PreLoader />
+      <div className="container mx-auto px-6">
+        <Navbar />
+        <App />
+        <Footer />
+      </div>
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PreLoader/>
-    <div className = "container mx-auto px-6">
-      <Navbar />
-      <App />
-      <Footer/>
-    </div>
-  </StrictMode>,
+    <Root />
+  </StrictMode>
 )
